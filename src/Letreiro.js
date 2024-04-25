0a0617b6a9406f0672b4fa9375e13a073171eb95
Giulia Kaori Nakamashi <126498272+giuliakaori@users.noreply.github.com>
2024-04-25 17:12:15 -0300

    Update Letreiro.js

--- src/Letreiro.js
+++ src/Letreiro.js
@@ -1,7 +1,25 @@
 function Letreiro() {
+    const [text, setText] = useState('');
+  const originalText = 'Conheça a Fatec';
+
+  useEffect(() => {
+    let index = 0;
+
+    const typingInterval = setInterval(() => {
+      setText((prevText) => {
+        const nextChar = originalText[index];
+        index = (index + 1) % originalText.length;
+        return prevText + nextChar;
+      });
+    }, 200); // Adjust typing speed here (milliseconds)
+
+    return () => clearInterval(typingInterval);
+  }, []);
+
     return (
       <>
           <h1>Meu Letreiro</h1>
+          <p>{texto}</p>
       </>
     );
   }
