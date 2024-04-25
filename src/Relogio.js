9eb74f083b67eb56cfae126021bb288a8c9b5708
Giulia Kaori Nakamashi <126498272+giuliakaori@users.noreply.github.com>
2024-04-25 14:47:13 -0300

    Update Relogio.js

--- src/Relogio.js
+++ src/Relogio.js
@@ -1,8 +1,21 @@
 function Relogio() {
+  const [dateTime, setDateTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDateTime(new Date());
+    }, 1000); // Atualiza a cada segundo
+
+    return () => clearInterval(interval); // Limpa o intervalo quando o componente é desmontado
+  }, []);
+
   return (
     <>
         <h1>Meu Relógio</h1>
-      
+        <div>
+        <p>Data: {dateTime.toLocaleDateString()}</p>
+        <p>Hora: {dateTime.toLocaleTimeString()}</p>
+      </div>
     </>
   );
 }
