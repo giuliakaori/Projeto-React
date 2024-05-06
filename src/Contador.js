887d429ea9e3d25013f4f7f8ad03ec7925862d89
Giulia Kaori Nakamashi <126498272+giuliakaori@users.noreply.github.com>
2024-05-06 14:17:33 -0300

    Update Contador.js

--- src/Contador.js
+++ src/Contador.js
@@ -1,1 +1,24 @@
+import React, { useState, useEffect } from 'react';
+function Relogio() {
+  const [dateTime, setDateTime] = useState(new Date());
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDateTime(new Date());
+    }, 1000); 
+
+    return () => clearInterval(interval); 
+  }, []);
+
+  return (
+    <>
+        <h1>Meu Relógio</h1>
+        <div>
+        <p>Data: {dateTime.toLocaleDateString()}</p>
+        <p>Hora: {dateTime.toLocaleTimeString()}</p>
+      </div>
+    </>
+  );
+}
+
+export default Relogio;
