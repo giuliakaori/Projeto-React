1f0ac10d39277c5d62ecd725436556533f1ffba1
Giulia Kaori Nakamashi <126498272+giuliakaori@users.noreply.github.com>
2024-05-09 11:34:35 -0300

    Update Atv03.js

--- src/Atv03.js
+++ src/Atv03.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function Atv02() {
     return (
         <>
-            <h1>Atividade 02</h1>
+            <h1>Atividade 03</h1>
             <Componente/>
             <br />
             <Link to="/">retornar a página inicial</Link>
