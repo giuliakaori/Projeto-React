1a67848505d2c4e14813bf888f4df58ff90c8fc5
Giulia Kaori Nakamashi <126498272+giuliakaori@users.noreply.github.com>
2024-05-09 10:18:37 -0300

    Update Atv03.js

--- src/Atv03.js
+++ src/Atv03.js
@@ -1,1 +1,13 @@
+import Componente from "./Componente";
+import { Link } from "react-router-dom";
 
+export default function Atv02() {
+    return (
+        <>
+            <h1>Atividade 02</h1>
+            <Componente/>
+            <br />
+            <Link to="/">retornar a página inicial</Link>
+        </>
+    );
+}
