8bbd5e0110f52fd706abe1b2af61e8ca3f7eae31
Giulia Kaori Nakamashi <126498272+giuliakaori@users.noreply.github.com>
2024-05-09 11:29:56 -0300

    Update Componente.js

--- src/Componente.js
+++ src/Componente.js
@@ -1,1 +1,14 @@
 import React from 'react';
+
+export default function User() {
+    return (
+        <article>
+            <h1>My First Component</h1>
+            <ol>
+                <li>Components: UI Building Blocks</li>
+                <li>Defining a Component</li>
+                <li>Using a Component</li>
+            </ol>
+        </article>
+    );
+}
