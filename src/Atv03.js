35e8340f80044bdd33c39168d9481eb17c13e6e5
Giulia Kaori Nakamashi <126498272+giuliakaori@users.noreply.github.com>
2024-05-09 12:25:09 -0300

    Update Atv03.js

--- src/Atv03.js
+++ src/Atv03.js
@@ -1,4 +1,14 @@
 import Componente from "./Componente";
+import Profile from './Gallery';
+import Scientist from './Scientist';
+import User from './Componente';
+import Avatar from './Avatar';
+import Avatar1 from './Avatar1';
+import NameDate from './NameDate';
+import Person from './Person';
+import Person1 from './Person1';
+import Avatar2 from './Avatar2';
+import Avatar3 from './Avatar3';
 import { Link } from "react-router-dom";
 
 export default function Atv03() {
@@ -6,6 +16,16 @@ export default function Atv03() {
         <>
             <h1>Atividade 03</h1>
             <Componente/>
+            <User/>
+            <Profile/>
+            <Scientist/>
+            <Avatar/>
+            <Avatar1/>
+            <NameDate/>
+            <Person/>
+            <Person1/>
+            <Avatar2/>
+            <Avatar3/>
             <br />
             <Link to="/">retornar a página inicial</Link>
         </>
