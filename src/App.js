cf7eb52abc96a15d079a9ef0361d846a6391643f
Giulia Kaori Nakamashi <126498272+giuliakaori@users.noreply.github.com>
2024-05-10 10:42:44 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    
+    <Componente/>
+            <User/>
+            <Profile/>
+            <Scientist/>
+            <Avatar/>
+            <Avatar1/>
+            <NameDate/>
+            <Person/>
+            <Person1/>
+            <Avatar2/>
+            <Avatar3/>
   );
 }
 
