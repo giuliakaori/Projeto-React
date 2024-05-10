5ccf5d8b34b68f3cbce6d6901119830479b93dce
Giulia Kaori Nakamashi <126498272+giuliakaori@users.noreply.github.com>
2024-05-10 10:43:13 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import logo from './logo.svg';
-
+import Componente from "./Componente";
+import Profile from './Gallery';
+import Scientist from './Scientist';
+import User from './Componente';
+import Avatar from './Avatar';
+import Avatar1 from './Avatar1';
+import NameDate from './NameDate';
+import Person from './Person';
+import Person1 from './Person1';
+import Avatar2 from './Avatar2';
+import Avatar3 from './Avatar3';
 import './App.css';
 
 function App() {
