2cd56aaade884b4a3ec17981bd6c55545c4334fa
Giulia Kaori Nakamashi <126498272+giuliakaori@users.noreply.github.com>
2024-05-10 16:47:24 -0300

    Update Atv03.js

--- src/Atv03.js
+++ src/Atv03.js
@@ -9,6 +9,19 @@ import Person1 from './Person1';
 import Avatar2 from './Avatar2';
 import Avatar3 from './Avatar3';
 import Children from './Children';
+import Item from './Item';
+import Item1 from './Item1';
+import Item2 from './Item2';
+import Item3 from './Item3';
+import Item4 from './Item4';
+import Item5 from './Item5';
+import Item6 from './Item6';
+import People from './People';
+import Person from './Person';
+import Recipe from './Recipe';
+import Cup from './Cup';
+import Teacup1 from './Teacup1';
+import Tea from './Tea';
 
 import { Link } from "react-router-dom";
 
@@ -27,6 +40,19 @@ export default function Atv03() {
             <Avatar2/>
             <Avatar3/>
             <Children/>
+            <Item/>
+            <Item1/>
+            <Item2/>
+            <Item3/>
+            <Item4/>
+            <Item5/>
+            <Item6/>
+            <People/>
+            <Person/>
+            <Recipe/>
+            <Cup/>
+            <Teacup1/>
+            <Tea/>
             <br />
             <Link to="/">retornar a página inicial</Link>
         </>
