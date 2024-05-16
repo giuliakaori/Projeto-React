f10d11cad7c65793b87a941ac02b4ea2362778f4
Giulia Kaori Nakamashi <126498272+giuliakaori@users.noreply.github.com>
2024-05-16 18:02:32 -0300

    Update Atv04.js

--- src/Atv04.js
+++ src/Atv04.js
@@ -4,6 +4,13 @@ import Counter from './Counter';
 import Form from './Form';
 import MovingDot from './MovingDot';
 import Form2 from './Form2';
+import Form3 from './Form3';
+import List from './List';
+import List2 from './List2';
+import ShapeEditor from './ShapeEditor';
+import CounterList from './CounterList';
+import List3 from './List3';
+import List4 from './List4';
 import { Link } from "react-router-dom";
 
 export default function Atv04() {
@@ -22,6 +29,22 @@ export default function Atv04() {
             <MovingDot/>
             <br/>
             <Form2/>
+            <br/>
+            <Form3/>
+            <br/>
+            <List/>
+            <br/>
+            <List2/>
+            <br/>
+            <ShapeEditor/>
+            <br/>
+            <CounterList/>
+            <br/>
+            <List3/>
+            <br/>
+            <List4/>
+            <br/>
+            <BucketList/>
             </>
             <Link to="/">retornar a página inicial</Link>
         </>
