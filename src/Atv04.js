6f4b49eb5ed156b9ca483e6690f65432c76dd928
Giulia Kaori Nakamashi <126498272+giuliakaori@users.noreply.github.com>
2024-05-16 12:25:04 -0300

    Update Atv04.js

--- src/Atv04.js
+++ src/Atv04.js
@@ -1,1 +1,23 @@
+import { Link } from "react-router-dom";
 
+export default function Atv04() {
+    return (
+        <>
+            <h1>Atividade 04</h1>
+            <>
+            <Toolbar/>
+            <br/>
+            <Gallery1/>
+            <br/>
+            <Counter/>
+            <br/>
+            <Form/>
+            <br/>
+            <MovingDot/>
+            <br/>
+            <Form2/>
+            </>
+            <Link to="/">retornar a página inicial</Link>
+        </>
+    );
+}
