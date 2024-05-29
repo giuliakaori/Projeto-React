b77ae4a7397472417620d64cdfe542fd00a35413
Giulia Kaori Nakamashi <126498272+giuliakaori@users.noreply.github.com>
2024-05-29 15:57:35 -0300

    Update Atv05.js

--- src/Atv05.js
+++ src/Atv05.js
@@ -1,1 +1,13 @@
+import Projeto from "./Projeto";
+import { Link } from "react-router-dom";
 
+export default function Atv01() {
+    return (
+        <>
+            <h1>Atividade 05</h1>
+            <Projeto/>
+            <br />
+            <Link to="/">retornar a página inicial</Link>
+        </>
+    );
+}
